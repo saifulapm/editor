025df5e2a32690be658e428d7c9fcad0a5c9f08d
Saiful Islam <saiful.apm@gmail.com>
2021-09-18 07:45:14 +0600

    Refactor

---
+++ lua/core/utils.lua
@@ -0,0 +1,383 @@
+local M = {}
+
+M.close_buffer = function(bufexpr, force)
+   -- This is a modification of a NeoVim plugin from
+   -- Author: ojroques - Olivier Roques
+   -- Src: https://github.com/ojroques/nvim-bufdel
+   -- (Author has okayed copy-paste)
+
+   -- Options
+   local opts = {
+      next = "cycle", -- how to retrieve the next buffer
+      quit = false, -- exit when last buffer is deleted
+      --TODO make this a chadrc flag/option
+   }
+
+   -- ----------------
+   -- Helper functions
+   -- ----------------
+
+   -- Switch to buffer 'buf' on each window from list 'windows'
+   local function switch_buffer(windows, buf)
+      local cur_win = vim.fn.winnr()
+      for _, winid in ipairs(windows) do
+         vim.cmd(string.format("%d wincmd w", vim.fn.win_id2win(winid)))
+         vim.cmd(string.format("buffer %d", buf))
+      end
+      vim.cmd(string.format("%d wincmd w", cur_win)) -- return to original window
+   end
+
+   -- Select the first buffer with a number greater than given buffer
+   local function get_next_buf(buf)
+      local next = vim.fn.bufnr "#"
+      if opts.next == "alternate" and vim.fn.buflisted(next) == 1 then
+         return next
+      end
+      for i = 0, vim.fn.bufnr "$" - 1 do
+         next = (buf + i) % vim.fn.bufnr "$" + 1 -- will loop back to 1
+         if vim.fn.buflisted(next) == 1 then
+            return next
+         end
+      end
+   end
+
+   -- ----------------
+   -- End helper functions
+   -- ----------------
+
+   local buf = vim.fn.bufnr()
+   if vim.fn.buflisted(buf) == 0 then -- exit if buffer number is invalid
+      vim.cmd "close"
+      return
+   end
+
+   if #vim.fn.getbufinfo { buflisted = 1 } < 2 then
+      if opts.quit then
+         -- exit when there is only one buffer left
+         if force then
+            vim.cmd "qall!"
+         else
+            vim.cmd "confirm qall"
+         end
+         return
+      end
+
+      local chad_term, type = pcall(function()
+         return vim.api.nvim_buf_get_var(buf, "term_type")
+      end)
+
+      if chad_term then
+         -- Must be a window type
+         vim.cmd(string.format("setlocal nobl", buf))
+         vim.cmd "enew"
+         return
+      end
+      -- don't exit and create a new empty buffer
+      vim.cmd "enew"
+      vim.cmd "bp"
+   end
+
+   local next_buf = get_next_buf(buf)
+   local windows = vim.fn.getbufinfo(buf)[1].windows
+
+   -- force deletion of terminal buffers to avoid the prompt
+   if force or vim.fn.getbufvar(buf, "&buftype") == "terminal" then
+      local chad_term, type = pcall(function()
+         return vim.api.nvim_buf_get_var(buf, "term_type")
+      end)
+
+      -- TODO this scope is error prone, make resilient
+      if chad_term then
+         if type == "wind" then
+            -- hide from bufferline
+            vim.cmd(string.format("%d bufdo setlocal nobl", buf))
+            -- swtich to another buff
+            -- TODO switch to next bufffer, this works too
+            vim.cmd "BufferLineCycleNext"
+         else
+            local cur_win = vim.fn.winnr()
+            -- we can close this window
+            vim.cmd(string.format("%d wincmd c", cur_win))
+            return
+         end
+      else
+         switch_buffer(windows, next_buf)
+         vim.cmd(string.format("bd! %d", buf))
+      end
+   else
+      switch_buffer(windows, next_buf)
+      vim.cmd(string.format("silent! confirm bd %d", buf))
+   end
+   -- revert buffer switches if user has canceled deletion
+   if vim.fn.buflisted(buf) == 1 then
+      switch_buffer(windows, buf)
+   end
+end
+
+-- hide statusline
+-- tables fetched from load_config function
+M.hide_statusline = function()
+   local hidden = require("core.utils").load_config().ui.plugin.statusline.hidden
+   local shown = require("core.utils").load_config().ui.plugin.statusline.shown
+   local api = vim.api
+   local buftype = api.nvim_buf_get_option("%", "ft")
+
+   -- shown table from config has the highest priority
+   if vim.tbl_contains(shown, buftype) then
+      api.nvim_set_option("laststatus", 2)
+      return
+   end
+
+   if vim.tbl_contains(hidden, buftype) then
+      api.nvim_set_option("laststatus", 0)
+      return
+   else
+      api.nvim_set_option("laststatus", 2)
+   end
+end
+
+-- load config
+-- 1st arg = boolean - whether to force reload
+-- Modifies _G._CONFIG global variable
+M.load_config = function(reload)
+   -- only do the stuff below one time, otherwise just return the set config
+   if _G._CONFIG_CONTENTS ~= nil and not (reload or false) then
+      return _G._CONFIG_CONTENTS
+   end
+   local config = "config"
+   _G._CONFIG_CONTENTS = require(config)
+   return _G._CONFIG_CONTENTS
+end
+
+M.map = function(mode, keys, cmd, opt)
+   local options = { noremap = true, silent = true }
+   if opt then
+      options = vim.tbl_extend("force", options, opt)
+   end
+
+   -- all valid modes allowed for mappings
+   -- :h map-modes
+   local valid_modes = {
+      [""] = true,
+      ["n"] = true,
+      ["v"] = true,
+      ["s"] = true,
+      ["x"] = true,
+      ["o"] = true,
+      ["!"] = true,
+      ["i"] = true,
+      ["l"] = true,
+      ["c"] = true,
+      ["t"] = true,
+   }
+
+   -- helper function for M.map
+   -- can gives multiple modes and keys
+   local function map_wrapper(mode, lhs, rhs, options)
+      if type(lhs) == "table" then
+         for _, key in ipairs(lhs) do
+            map_wrapper(mode, key, rhs, options)
+         end
+      else
+         if type(mode) == "table" then
+            for _, m in ipairs(mode) do
+               map_wrapper(m, lhs, rhs, options)
+            end
+         else
+            if valid_modes[mode] and lhs and rhs then
+               vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+            else
+               mode, lhs, rhs = mode or "", lhs or "", rhs or ""
+               print("Cannot set mapping [ mode = '" .. mode .. "' | key = '" .. lhs .. "' | cmd = '" .. rhs .. "' ]")
+            end
+         end
+      end
+   end
+
+   map_wrapper(mode, keys, cmd, options)
+end
+
+-- load plugin after entering vim ui
+M.packer_lazy_load = function(plugin, timer)
+   if plugin then
+      timer = timer or 0
+      vim.defer_fn(function()
+         require("packer").loader(plugin)
+      end, timer)
+   end
+end
+
+-- clear command line from lua
+M.clear_cmdline = function()
+   vim.defer_fn(function()
+      vim.cmd "echo"
+   end, 0)
+end
+
+-- wrapper to use vim.api.nvim_echo
+-- table of {string, highlight}
+-- e.g echo({{"Hello", "Title"}, {"World"}})
+M.echo = function(opts)
+   if opts == nil or type(opts) ~= "table" then
+      return
+   end
+   vim.api.nvim_echo(opts, false, {})
+end
+
+-- 1st arg - r or w
+-- 2nd arg - file path
+-- 3rd arg - content if 1st arg is w
+-- return file data on read, nothing on write
+M.file = function(mode, filepath, content)
+   local data
+   local fd = assert(vim.loop.fs_open(filepath, mode, 438))
+   local stat = assert(vim.loop.fs_fstat(fd))
+   if stat.type ~= "file" then
+      data = false
+   else
+      if mode == "r" then
+         data = assert(vim.loop.fs_read(fd, stat.size, 0))
+      else
+         assert(vim.loop.fs_write(fd, content, 0))
+         data = true
+      end
+   end
+   assert(vim.loop.fs_close(fd))
+   return data
+end
+
+-- return a table of available themes
+M.list_themes = function(return_type)
+   local themes = {}
+   -- folder where theme files are stored
+   local themes_folder = vim.fn.stdpath "data" .. "/site/pack/packer/opt/nvim-base16.lua/lua/hl_themes"
+   -- list all the contents of the folder and filter out files with .lua extension, then append to themes table
+   local fd = vim.loop.fs_scandir(themes_folder)
+   if fd then
+      while true do
+         local name, typ = vim.loop.fs_scandir_next(fd)
+         if name == nil then
+            break
+         end
+         if typ ~= "directory" and string.find(name, ".lua$") then
+            -- return the table values as keys if specified
+            if return_type == "keys_as_value" then
+               themes[vim.fn.fnamemodify(name, ":r")] = true
+            else
+               table.insert(themes, vim.fn.fnamemodify(name, ":r"))
+            end
+         end
+      end
+   end
+   return themes
+end
+
+-- reload a plugin ( will try to load even if not loaded)
+-- can take a string or list ( table )
+-- return true or false
+M.reload_plugin = function(plugins)
+   local status = true
+   local function _reload_plugin(plugin)
+      local loaded = package.loaded[plugin]
+      if loaded then
+         package.loaded[plugin] = nil
+      end
+      local ok, err = pcall(require, plugin)
+      if not ok then
+         print("Error: Cannot load " .. plugin .. " plugin!\n" .. err .. "\n")
+         status = false
+      end
+   end
+
+   if type(plugins) == "string" then
+      _reload_plugin(plugins)
+   elseif type(plugins) == "table" then
+      for _, plugin in ipairs(plugins) do
+         _reload_plugin(plugin)
+      end
+   end
+   return status
+end
+
+-- Edit user config file, based on the assumption it exists in the config as
+-- theme = "theme name"
+-- 1st arg as current theme, 2nd as new theme
+M.change_theme = function (current_theme, new_theme)
+   if current_theme == nil or new_theme == nil then
+      print "Error: Provide current and new theme name"
+      return false
+   end
+   if current_theme == new_theme then
+      return
+   end
+
+   local file_fn = require("core.utils").file
+   local user_config = vim.g.main_config
+   local file = vim.fn.stdpath "config" .. "/lua/" .. user_config .. ".lua"
+   -- store in data variable
+   local data = assert(file_fn("r", file))
+   -- escape characters which can be parsed as magic chars
+   current_theme = current_theme:gsub("%p", "%%%0")
+   new_theme = new_theme:gsub("%p", "%%%0")
+   local find = "theme = .?" .. current_theme .. ".?"
+   local replace = 'theme = "' .. new_theme .. '"'
+   local content = string.gsub(data, find, replace)
+   -- see if the find string exists in file
+   if content == data then
+      print("Error: Cannot change default theme with " .. new_theme .. ", edit " .. file .. " manually")
+      return false
+   else
+      assert(file_fn("w", file, content))
+   end
+end
+
+-- reload themes without restarting vim
+-- if no theme name given then reload the current theme
+M.reload_theme = function (theme_name)
+   local reload_plugin = require("core.utils").reload_plugin
+
+   -- if theme name is empty or nil, then reload the current theme
+   if theme_name == nil or theme_name == "" then
+      theme_name = vim.g.main_theme
+   end
+
+   if not pcall(require, "hl_themes." .. theme_name) then
+      print("No such theme ( " .. theme_name .. " )")
+      return false
+   end
+
+   vim.g.main_theme = theme_name
+
+   -- reload the base16 theme and highlights
+   require("colors").init(theme_name)
+
+   if not reload_plugin {
+      "plugins.configs.bufferline",
+      "plugins.configs.statusline",
+   } then
+      print "Error: Not able to reload all plugins."
+      return false
+   end
+
+   return true
+end
+
+-- toggle between 2 themes
+-- argument should be a table with 2 theme names
+M.toggle_theme = function (themes)
+   local current_theme = vim.g.current_main_theme or vim.g.main_theme
+   for _, name in ipairs(themes) do
+      if name ~= current_theme then
+         if require("core.utils").reload_theme(name) then
+            -- open a buffer and close it to reload the statusline
+            vim.cmd "new|bwipeout"
+            vim.g.current_main_theme = name
+            if require("core.utils").change_theme(vim.g.main_theme, name) then
+               vim.g.main_theme = name
+            end
+         end
+      end
+   end
+end
+
+return M
